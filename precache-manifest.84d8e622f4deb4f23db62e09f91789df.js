a7a6114bf0be35d7cf2eef53fa487e41cc8411d5
Zetra Betfrex <zetrabetfrex@gmail.com>
2020-08-04 20:35:54 +0530

    Updates

---
+++ precache-manifest.84d8e622f4deb4f23db62e09f91789df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b34cfa807ec727aad2bde111980dd31",
+    "url": "/monster-rolodex1/index.html"
+  },
+  {
+    "revision": "e50860e05ae931f1204f",
+    "url": "/monster-rolodex1/static/css/main.b8cd18ea.chunk.css"
+  },
+  {
+    "revision": "77263cff91b03fe4e343",
+    "url": "/monster-rolodex1/static/js/2.7b866b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex1/static/js/2.7b866b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50860e05ae931f1204f",
+    "url": "/monster-rolodex1/static/js/main.8e5c3ce7.chunk.js"
+  },
+  {
+    "revision": "a5991477fcba4de9e785",
+    "url": "/monster-rolodex1/static/js/runtime-main.9287e28d.js"
+  }
+]);
